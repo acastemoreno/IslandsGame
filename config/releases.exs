576a267fd30ee14aa2c1c397dd7f2b2d269f3446
Albert Castellano Moreno <albert@able.co>
2020-12-11 10:15:34 -0500

    release config

--- config/releases.exs
+++ config/releases.exs
@@ -6,4 +6,7 @@ config :islands_interface, IslandsInterfaceWeb.Endpoint,
   load_from_system_env: true,
   # url: [host: "example.com", port: 80],
   check_origin: false,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  server: true,
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
