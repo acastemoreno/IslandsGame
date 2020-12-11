e54bf4626c0c97190444d8e5c2155ed7f44b926e
Albert Castellano Moreno <albert@able.co>
2020-12-11 10:20:30 -0500

    release config release

--- mix.exs
+++ mix.exs
@@ -6,7 +6,15 @@ defmodule IslandsGame.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      releases: [
+        islands_engine: [
+          applications: [islands_engine: :permanent]
+        ],
+        islands_interface: [
+          applications: [islands_interface: :permanent]
+        ]
+      ]
     ]
   end
 
