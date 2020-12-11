642437a2887eeda28c0fdc8e414d14d242884357
Albert Castellano Moreno <albert@able.co>
2020-12-11 10:05:36 -0500

    Fixing css

--- config/config.exs
+++ config/config.exs
@@ -11,17 +11,3 @@ import Mix.Config
 
 # import_config "../apps/islands_engine/config/config.exs"
 import_config "../apps/islands_interface/config/config.exs"
-
-config :islands_interface, IslandsInterfaceWeb.Endpoint,
-  load_from_system_env: true,
-  # url: [host: "example.com", port: 80],
-  check_origin: false,
-  cache_static_manifest: "priv/static/cache_manifest.json"
-
-# Sample configuration:
-#
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
-#
