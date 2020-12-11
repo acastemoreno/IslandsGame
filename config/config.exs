83e3801ac7a085b97064e53d0a3d32beb802386c
Albert Castellano Moreno <albert@able.co>
2020-12-11 10:36:55 -0500

    Back to mix2

--- config/config.exs
+++ config/config.exs
@@ -16,4 +16,4 @@ config :islands_interface, IslandsInterfaceWeb.Endpoint,
   load_from_system_env: true,
   # url: [host: "example.com", port: 80],
   check_origin: false,
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json"
